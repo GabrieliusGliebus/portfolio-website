c92dc40625bc17831e054ede0f339c85458d8112
GabrieliusGliebus <gabrieliusdarb@gmail.com>
2026-06-30 21:09:12 +0900

    Update LandingPageIntro.js

--- src/Texts/LandingPageIntro.js
+++ src/Texts/LandingPageIntro.js
@@ -1,5 +1,5 @@
 export  const Intro ={
-    title: "I work on game and software projects, mostly around Unity, Unreal Engine, and mobile development.",
+    title: "I work on game and software projects, mainly on Unity, Unreal Engine, and mobile development.",
 }
 
 export const mostRecentProject = {
